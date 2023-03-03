7d9bdb9361162d142d8f7550ac39aa1b73ad8e39
toshixtriplesix <125659361+toshixtriplesix@users.noreply.github.com>
2023-03-03 16:39:02 +0200

    Modificari Lab3

--- lab2caramusca/lab2/src/App.tsx
+++ lab2caramusca/lab2/src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Layout, Menu, Card, Form, Input, Button } from "antd";
+
+const { Header, Content } = Layout;
 
 function App() {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [input3, setInput3] = useState("");
+
+  const handleButtonClick = () => {
+    console.log("Inputs:", input1, input2, input3);
+    alert("Inputs: " + input1 + ", " + input2 + ", " + input3);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code>Caramusca Anton Lab 2 CR-212</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">Info</Menu.Item>
+          <Menu.Item key="3">Help</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "50px" }}>
+        <Card title="Kentu 1">Caramusca Anton</Card>
+        <Card title="Kentu 2">Marin Suvac</Card>
+        <Card title="Kentu 3">Varzari Nicolae</Card>
+        <Form style={{ marginTop: "50px" }}>
+          <Form.Item label="Ce spune Toha">
+            <Input
+              value={input1}
+              onChange={(e) => setInput1(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item label="Ce spune Marin">
+            <Input
+              value={input2}
+              onChange={(e) => setInput2(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item label="Ce spune Colea">
+            <Input
+              value={input3}
+              onChange={(e) => setInput3(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" onClick={handleButtonClick}>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Content>
+    </Layout>
   );
 }
 
